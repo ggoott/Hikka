64f92f88210de6328b83e04fbe9c482f252cfd81
ggoott <88576648+ggoott@users.noreply.github.com>
2024-10-08 11:49:52 +0600

    Update hikka_info.py

--- hikka/modules/hikka_info.py
+++ hikka/modules/hikka_info.py
@@ -26,7 +26,7 @@ class HikkaInfoMod(loader.Module):
             ),
             loader.ConfigValue(
                 "custom_button",
-                ["🌘 Support chat", "https://t.me/hikka_talks"],
+                ["AmeFork", "https://youtube.com/"],
                 lambda: self.strings("_cfg_cst_btn"),
                 validator=loader.validators.Union(
                     loader.validators.Series(fixed_len=2),
@@ -78,7 +78,7 @@ class HikkaInfoMod(loader.Module):
 
         return (
             (
-                "<b>🌘 Hikka</b>\n"
+                "<b>💠 AmeFork</b>\n"
                 if "hikka" not in self.config["custom_message"].lower()
                 else ""
             )
