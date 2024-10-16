b25f08ff168ec81883aa954d613c292e9f2ed188
ggoott <88576648+ggoott@users.noreply.github.com>
2024-10-16 19:47:39 +0500

    Update main.py

--- hikka/main.py
+++ hikka/main.py
@@ -785,7 +785,7 @@ class Hikka:
                 "https://0x0.st/s/-76GFdh-PuPaiw4pnM-yGQ/XIbn.mp4",
                 caption=(
                     "╭╶╶╶╶╶╶╶╶╶╶╶╶╶╶╶╶╶╮\n╵\n╵    🐾 <b>𝙷𝚒𝚔𝚔𝚊 {} 𝚂𝚝𝚊𝚛𝚝𝚎𝚍!</b>\n╵     ╴╴╴╴╴╴╴╴╴╴╴╴\n╵\n╵    ✨ <b>𝙶𝚒𝚝𝙷𝚞𝚋 𝚌𝚘𝚖𝚖𝚒𝚝 𝚂𝙷𝙰: <a"
-                    ' href="https://github.com/ggoott/Hikka/commit/{}">{}</a></b>\n✊'
+                    ' href="https://github.com/ggoott/Hikka/commit/{}">{}</a></b>'
                     "\n╵    🎯 <b>𝚄𝚙𝚍𝚊𝚝𝚎 𝚜𝚝𝚊𝚝𝚞𝚜: {}</b>\n╵    <b>{}</b>\n╵\n╵🔥 𝙵𝚘𝚛𝚔𝚎𝚍 𝚋𝚢: @amm1e\n╰╶╶╶╶╶╶╶╶╶╶╶╶╶╶╶╶╶╯".format(
                         ".".join(list(map(str, list(__version__)))),
                         build,
